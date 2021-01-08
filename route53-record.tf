f92b852b12c00db9636dc8dba8aaf88438211b49
José Roberto Prado <jrpradojunior@gmail.com>
2021-01-08 19:35:23 -0300

    Initial

---
+++ route53-record.tf
@@ -0,0 +1,14 @@
+data "aws_route53_zone" "selected" {
+  name = var.hosted_zone
+}
+
+resource "aws_route53_record" "hostname" {
+  count = var.hostname_create ? length(var.hostnames) : 0
+
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.hostnames[count.index]
+  type    = "CNAME"
+  ttl     = "300"
+  records = list(element(aws_cloudfront_distribution.default.*.domain_name, 0))
+}
+
